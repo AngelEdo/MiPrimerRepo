d04c392c2c5d070cb4b44975dd5e4ab026f38fa7
AngelEdo <angeldecarlo121416@gmail.com>
2025-05-10 23:40:47 -0600

    cambio de nombre

--- proyeto1.cpp
+++ proyeto1.cpp
@@ -4,24 +4,7 @@ using namespace std;
 
 int main(){
 
-    const char *cadena = "Hola mundo";
-    char cadena2[] = "Hola mundo";
-
-    //primera forma de recorrer cadena 
-    cout << "\n\n Primera forma \n "; 
-    for (int i=0; cadena[i] != '\0'; i++)
-         cout << cadena[i] << "";
-
-    //segunda forma 
-    cout << "\n\n Segunda forma \n ";
-    for (int i=0; *(cadena+i) != '\0'; i++)
-       cout << *(cadena+i) <<"";
-
-    //segunda forma 
-    cout << "\n\n Tercer forma \n ";
-    for (; *cadena != '\0';cadena++)
-       cout << *cadena <<"";
-    
-
+    cout << "\n\n HOLA \n "; 
+   
     return 0;
 }
